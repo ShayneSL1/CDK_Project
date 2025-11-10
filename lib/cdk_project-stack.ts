92e03b5a91d7823c014c8a92bd11987b0a79ba02
ShayneSL1 <ssluangvisa@gmail.com>
2025-11-10 02:37:05 -0500

    Built a vpc with 2 AZs and related public and private subnets using the props section within the constructor of each resource

--- lib/cdk_project-stack.ts
+++ lib/cdk_project-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class CdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24
+        },
+      ],
+    }) 
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: vpc.vpcId,
+      description: 'VPC ID'
+    })
+  };
+};
